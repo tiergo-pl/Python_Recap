79d3d4b876a3fe038c78c0dc85e7fd060e4282ba
Tiergo <tiergo@tiergo.pl>
2022-04-01 15:53:00 +0200

    3

--- recap02.py
+++ recap02.py
@@ -1,8 +1,10 @@
 
-from operator import lshift
-
-
 default_filename = 'a.txt'
+output_filename='out.txt'
+
+def print_write_file(file_obj,print_args):
+    print(print_args)
+    print(print_args, file=file_obj)
 
 
 def read_text_from_file(filename):
@@ -11,6 +13,7 @@ def read_text_from_file(filename):
         filename = ans
     file = open(filename, 'r')
     file_contents = file.read()
+    file.close()
     return file_contents
 
 
@@ -32,29 +35,42 @@ def string_to_dict(string):
     count_list = []
     count = 0
     for element in out:
-        count_list.extend(str(count))
+        count_list.append(str(count))
         count = count+1
-    out2=dict(zip(count_list,out))
+    out2 = dict(zip(count_list, out))
     return out2
 
 
+def string_to_dict2(string):
+    string = string.replace("\n", " ")
+    out = string.split(' ')
+    count_list = list(range(len(out)))
+    out2 = dict(zip(count_list, out))
+    return out2
+
+
+out_file = open(output_filename,'w')
 answer = 'y'
 while answer == 'y' or answer == 'Y':
     text_data = read_text_from_file(default_filename)
-    print(text_data)
-    print('Convert text into:')
-    print('List:')
+    print_write_file(out_file,text_data)
+    print_write_file(out_file, 'Convert text into:')
+    print_write_file(out_file, 'List:')
     input_data = string_to_list(text_data)
-    print(f"input_data= {input_data}")
-    print('Tuple:')
+    print_write_file(out_file, f"input_data= {input_data}")
+    print_write_file(out_file, 'Tuple:')
     input_data = string_to_tuple(text_data)
-    print(f"input_data= {input_data}")
-    print('Dict:')
+    print_write_file(out_file, f"input_data= {input_data}")
+    print_write_file(out_file, 'Dict:')
     input_data = string_to_dict(text_data)
-    print(f"input_data= {input_data}")
+    print_write_file(out_file, f"input_data= {input_data}")
+    print_write_file(out_file, 'Dict2:')
+    input_data = string_to_dict2(text_data)
+    print_write_file(out_file, f"input_data= {input_data}")
 
     answer = input('Again? [Y/n]: ')
     if not answer:
         answer = 'y'
     print()
+out_file.close()
 print('Bye, bye!\n')
